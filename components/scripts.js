4491c0bd93687333d4e1f28dda25d73e3d1acc06
Duvvvy <ryanbircham@gmail.com>
2019-08-17 11:37:32 +1200

    Example data rendering

--- components/scripts.js
+++ components/scripts.js
@@ -1,8 +1,29 @@
 var cards = document.getElementsByClassName("EventCard")[0];
-var items = json.items;
-/*
+var items = [
+    {
+    "title": "accord",
+    "description": "focus",
+    "timeDate": "accord",
+    "location": "focus",
+    "link": "google.com"
+    },
+    {
+    "title": "accord2",
+    "description": "focus2",
+    "timeDate": "accord2",
+    "location": "focus2",
+    "link": "facebook.con"
+    },
+    {
+    "title": "accord3",
+    "description": "focus3",
+    "timeDate": "accord3",
+    "location": "focus3",
+    "link": "hypixel.net"
+    }
+]
 
-*/
+console.log(items);
 for(var i = 0; i < items.length; i++) {
     var divCard = document.createElement("div");
     var CardTitle = document.createElement("h1");//
@@ -11,6 +32,7 @@ for(var i = 0; i < items.length; i++) {
     var CardTime = document.createElement("p");//
     var CardLocation = document.createElement("p");//
     var Link = document.createElement("a");//
+
     CardTitle.innerHTML = items[i].title;
     CardDescription.innerHTML = items[i].description;
     CardTime.innerHTML = items[i].timedate;
