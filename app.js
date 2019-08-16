63186accf349b849ec48dedfcc87108c580037df
Minh-Phu <Lminhphu0304@gmail.com>
2019-08-17 11:46:40 +1200

    Minor changes

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 let Express = require('express');
+const path = require('path')
 let bodyParser = require('body-parser')
+
 let port = process.env.PORT || 3000;
 
 
 let app = Express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(Express.static('/'))
 app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'world'}));
-});
-
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 app.listen(port, ()=> {
     console.log(`Server is runing on port ${port}`)
