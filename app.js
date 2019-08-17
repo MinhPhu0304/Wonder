f26720463c0b5e09a3172a865f757f7ff02f91bc
Minh-Phu <Lminhphu0304@gmail.com>
2019-08-17 12:20:48 +1200

    Add dotenv for reading environment variable

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-let Express = require('express');
+const Express = require('express');
 const path = require('path')
-let bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+const eventbriteKey = process.env.EventBriteToken;
+const meetUpKey = process.env.MeetupToken
 
-let port = process.env.PORT || 3000;
-
-
-let app = Express();
+const app = Express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(Express.static('/'))
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
-})
+});
 
 app.listen(port, ()=> {
     console.log(`Server is runing on port ${port}`)
