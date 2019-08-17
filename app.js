a48262105618bb285337057c5431a7a6f609d0fc
Minh-Phu <Lminhphu0304@gmail.com>
2019-08-17 13:29:50 +1200

    Add api endpoint to list events

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const path = require('path')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const port = process.env.PORT || 3000;
-const eventbriteKey = process.env.EventBriteToken;
-const meetUpKey = process.env.MeetupToken
 
 const app = Express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(Express.static('/'))
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
+
+app.get('/api/events',(req, res) => {
+
+})
 
 app.listen(port, ()=> {
     console.log(`Server is runing on port ${port}`)
